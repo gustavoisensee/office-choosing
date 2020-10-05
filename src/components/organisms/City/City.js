90f2c1e447d613a09fa170f37a80c8a208c177e5
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-10-05 20:47:50 +0200

    feat: add arrow icon

--- src/components/organisms/City/City.js
+++ src/components/organisms/City/City.js
@@ -1,18 +1,22 @@
 import React, { Fragment, useState } from 'react';
 import CityItem from '../CityItem/CityItem';
+import arrowImg from '../../../assets/arrow.png';
 import styles from './City.module.css';
 
 const City = ({ name, nameForecast, flyFrom, flyTo, img }) => {
-  const [inProp, setInProp] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <Fragment>
-      <div className={styles.container} onClick={() => setInProp(!inProp)}>
-        <img src={img} alt='' className={styles.cityIcon} />
-        {name}
+      <div className={styles.container} onClick={() => setToggle(!toggle)}>
+        <div className={styles.titleContainer}>
+          <img src={img} alt='' className={styles.cityIcon} />
+          {name}
+        </div>
+        <img src={arrowImg} alt='' className={toggle ? styles.arrowIconOpen : styles.arrowIcon} />
       </div>
-      <div className={inProp ? styles.open : styles.closed}>
-        {inProp && (
+      <div className={toggle ? styles.open : styles.closed}>
+        {toggle && (
           <CityItem name={name} flyFrom={flyFrom} flyTo={flyTo} nameForecast={nameForecast} />
         )}
       </div>
