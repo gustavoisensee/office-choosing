ad1864db671221c7ed2e6989ca0d01aca412ecde
Gustavo Isensee <gustavo.isensee@gmail.com>
2024-02-16 00:10:52 +0100

    fix: test netlify proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/weather": {
+        target: "http://api.openweathermap.org",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/weather/, ''),
+      },
+    }
+  }
 })
